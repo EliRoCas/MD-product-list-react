18c08d6d26cea0a11c142eddcf225d53b64a7ed2
isabelaTV <isabellactv96@gmail.com>
2024-09-20 17:42:14 -0500

    se agrega nueva rama

--- src/elements/Description.jsx
+++ src/elements/Description.jsx
@@ -1,31 +1,9 @@
-import PropTypes from "prop-types";
-
-const Description = ({ description }) => {
-  return (
-    <div className="cardDescription">
-      <p>{description}</p>
-    </div>
-  );
-};
-
-Description.propTypes = {
-  description: PropTypes.string.isRequired,
-};
-
-export default Description;
-
-// Prueba de funcionamiento del elemento atómico Description
 // import PropTypes from "prop-types";
 
-// const Description = () => {
+// const Description = ({ description }) => {
 //   return (
 //     <div className="cardDescription">
-//       <p>
-//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, suscipit.
-//         Culpa amet quis aperiam, omnis expedita eos commodi voluptatibus
-//         architecto dolores blanditiis! Incidunt vitae cupiditate, repellat minus
-//         placeat autem itaque?
-//       </p>
+//       <p>{description}</p>
 //     </div>
 //   );
 // };
@@ -35,3 +13,25 @@ export default Description;
 // };
 
 // export default Description;
+
+// Prueba de funcionamiento del elemento atómico Description
+import PropTypes from "prop-types";
+
+const Description = () => {
+  return (
+    <div className="cardDescription">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, suscipit.
+        Culpa amet quis aperiam, omnis expedita eos commodi voluptatibus
+        architecto dolores blanditiis! Incidunt vitae cupiditate, repellat minus
+        placeat autem itaque?
+      </p>
+    </div>
+  );
+};
+
+Description.propTypes = {
+  description: PropTypes.string.isRequired,
+};
+
+export default Description;
