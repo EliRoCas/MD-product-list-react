96700e61a8eac2f6fd18438b0e35d24d1c8ed878
Eli RoCas <elir_94@hotmail.com>
2024-09-24 20:49:40 -0500

    add: general layout

---
+++ src/sections/header/Header.jsx
@@ -0,0 +1,43 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+import "./header.scss";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+function OffcanvasExample() {
+  const expand = "lg";
+
+  return (
+    <>
+      <Navbar key={expand} expand={expand} className="nav">
+        <Container fluid>
+          <Navbar.Brand href="#">Random Rhapsody</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Random Rhapsody
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Button>Home</Button>
+                <Button>Card</Button>
+                <Button>Form</Button>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default OffcanvasExample;
