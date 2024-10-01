9de2de7784e2fbee4bd3754941fbf59bda95a785
isabelaTV <isabellactv96@gmail.com>
2024-10-01 16:54:45 -0500

    se corrigio archivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Layout from "./layout/Layout";
@@ -7,21 +6,14 @@ import FormScreen from "./screens/FormScreen";
 import CardListScreen from "./screens/CardListScreen";
 import HomeScreen from "./screens/HomeScreen"
 
-
 function App() {
-  const [cards, setCards] = useState([]);
-
-  const addItem = (item) => {
-    setCards((prevCards) => [item, ...prevCards]);
-  };
-
   return (
     <BrowserRouter>
         <Layout>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
-            <Route path="/cards" element={< CardListScreen cards={cards} setCards={setCards} />} />
-            <Route path="/form" element={< FormScreen addItem={addItem}/>} />
+            <Route path="/cards" element={< CardListScreen/>} />
+            <Route path="/form" element={< FormScreen/>} />
           </Routes>
         </Layout>
     </BrowserRouter>
